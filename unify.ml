d97e6612c03a8f5d2444a8cad4b165cfa0a2081f
hx312 <646544734@qq.com>
2014-11-23 18:17:15 -0600

    change notation of ast

--- unify.ml
+++ unify.ml
@@ -1,14 +1,4 @@
-type arg = VarArg of string | ConstArg of const and
-const = 
-BoolConst of bool 
-| IntConst of int
-| FloatConst of float
-| StringConst of string
-| NilConst
-| UnitConst;;
-
-type term = AtomTerm of arg | CompoundTerm of (string * arg list) |
-            ListTerm of term list;;
+open ProjCommon
 
 
 
