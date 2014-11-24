78d1ce29deb44639beef49f2f7f2a6e22089f31f
hx312 <646544734@qq.com>
2014-11-23 18:18:45 -0600

    change structure of the proj

---
+++ projCommon.ml
@@ -0,0 +1,11 @@
+type const = 
+BoolConst of bool 
+| IntConst of int
+| FloatConst of float
+| StringConst of string
+| NilConst
+| UnitConst;;
+
+type term = Var of string | ConstTerm of const |
+		CompoundTerm of (string * term list) |
+            ListTerm of term list;;
