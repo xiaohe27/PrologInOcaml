3e899d0946ab1bd6a6ecf1626b5986683a276279
laurayuwen <laurayuwen@gmail.com>
2014-11-23 18:00:35 -0600

    Add mp8 for reference

---
+++ mp8common.ml
@@ -0,0 +1,56 @@
+(* File: mp8common.ml *)
+
+type token =
+  | INT of (int)
+  | FLOAT of (float)
+  | BOOL of (bool)
+  | STRING of (string)
+  | IDENT of (string)
+  | NEG
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIV
+  | DPLUS
+  | DMINUS
+  | DTIMES
+  | DDIV
+  | CARAT
+  | LT
+  | GT
+  | LEQ
+  | GEQ
+  | EQUALS
+  | NEQ
+  | PIPE
+  | ARROW
+  | SEMI
+  | DSEMI
+  | DCOLON
+  | AT
+  | NIL
+  | LET
+  | REC
+  | AND
+  | END
+  | IN
+  | IF
+  | THEN
+  | ELSE
+  | FUN
+  | MOD
+  | RAISE
+  | TRY
+  | WITH
+  | NOT
+  | LOGICALAND
+  | LOGICALOR
+  | LBRAC
+  | RBRAC
+  | LPAREN
+  | RPAREN
+  | COMMA
+  | UNDERSCORE
+  | UNIT
+  | EOF
+
