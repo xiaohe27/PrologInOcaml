c36aa2f802d475190170cbbf4107492c62e77c13
He Xiao <hexiao2@illinois.edu>
2014-11-28 00:38:34 -0600

    modify clause and query to enable predicates to be connected by both comma and semi-colon

--- projCommon.ml
+++ projCommon.ml
@@ -12,8 +12,9 @@ type term = Var of string | ConstTerm of const |
 
 type predicate = Identifier of string | Predicate of string * (term list);;
 
-type clause = Fact of predicate | Rule of predicate * (predicate list);;
+		(* predicates can either be separated by comma or by semi-colon *)
+type clause = Fact of predicate | Rule of predicate * (predicate list * string list);;
 
-type query = Query of predicate list;;
+type query = Query of (predicate list * string list);;
 
 type program = Prog of clause list * query | ProgFromQuery of query;;
